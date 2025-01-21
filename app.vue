2331a0873e5258f8f58ad8a58f476288fe461a95
Raaydan <ilsur2000.2016@gmail.com>
2025-01-21 20:05:39 +0300

    deploy

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
-  <Html class="scroll-smooth h-screen">
-    <Body class="flex flex-col w-full h-full">
+  <Html class="scroll-smooth">
+    <Body class="flex flex-col w-full min-h-screen">
       <HeaderVue />
-      <main class="px-4 md:px-8 h-full">
+      <main class="px-4 md:px-8 h-full flex-1">
         <NuxtPage />
       </main>
       <FooterVue />
